525f62ab03760ed6618815d4e3cc9f80ee0ebf1a
xuefengwang <xuefengwang@Ctrip.com>
2017-07-04 13:50:30 +0800

    更换为原生java加密方法

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,38 @@ public class Main {
 //        String md5String = Hashing.md5().hashBytes("[{\"QuestionGuid\":\"a3228d15-d812-42ce-a15c-6b88bf7c002b\",\"Content\":\"asdas\",\"GroupCode\":\"HotelLocal\",\"CustomType\":\"Channel\"}]".concat("809d6e0be55d48be9a94a1cf39c7d34e").getBytes()).toString();
 //        System.out.println(md5String);
 
-        SecurityRsaKeyPair keyPair =  SecurityKeyGenerator.Generate();
-        System.out.println(keyPair);
+//        SecurityRsaKeyPair keyPair =  SecurityKeyGenerator.Generate();
+//        System.out.println(keyPair);
+//
+//        String data = "HelloWorld";
+//        try{
+//            String encrypted = Security.RSAEncrypt(keyPair.getPublicKey(),data);
+//            String decrypted = Security.RSADecrypt(keyPair.getPrivateKey(),encrypted);
+//            System.out.println(decrypted);
+//        }catch(Exception exception){
+//            exception.printStackTrace();
+//        }
+
+
+//        try {
+//            String netPublicKey = "MIGdMA0GCSqGSIb3DQEBAQUAA4GLADCBhwKBgQCHf0TtJvM+2tsLDuvTuHkr3OZXI6k7pA7CFkKdJSczuzJN5sG84XIeBnzsZbsoaL962RVcyUhxGxkibxlIdvZ4z8ie2Ren2CwHMQhQJSKkyt+RpijZIMg8ZPF65x4ya2Q08LhzG9jZbGkuFeukd4pCzY6T65F96VSOafXafhlWzwIBAw==";
+//            String encrypted = Security.RSAEncrypt(netPublicKey, data);
+//            System.out.println("Java Use Net PublicKey");
+//            System.out.println(encrypted);
+//
+//
+//        } catch (Exception exception) {
+//            exception.printStackTrace();
+//        }
+
+        try{
+            String encrypted = "S/5vlgjiKrsb8zuTaR7375GuyAfdUTONPOh6SxBL9nELdrpyKpIjWSf6ZxmWESMjLxxcv4ri336et/E36bhGbrOvzwqQPMluO8NVWspt6qZaDw5bd5DA5Gd7XsduEvtQtqIAXaGINepA01kbO/ZGI1+yuMYXHcrAWWQYVkcffWc=";
+            String javaPrivateKey ="MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAN4ii2bSjgHk8yIVO+9d3tpf4llAR3hgIhyvWRfDx4OglLMEIkxJNycy+CqrfDaBCdq+/LH2r5z54wpW/l6TgvlzhVnvv/aDDZaJmfHbatr4wDR56D9cwvFmjhKBN6I/lnOKtweUTBge7QcIIN9COZ6V5b+HWZnyZRpEYiarVE/ZAgEDAoGAJQXB5nhtAFDTMFjfUo+lJGVQZDVhPrqwWh05g/X2lfAYyICwYgwz293UBxyUs8AsTx/UyFPH7377LGPVD8NAfpkR0pXlxBiyFK5Blsb+GGrzSR9SLyRUaevz5FtaLfe8oLrvE+lGPoXn2/myIm0gsSzu+E3YKuv2dK1L5jY6/4kCQQD05rf+bY9l8Hasf3CuQpemZgvWR9zsel5ndn2zoTput0uu1c2jXeWJPmV7wgOs43D6A1IzxZZLSY9WdfsWXJD3AkEA6DOybe/OiPCjx5D4gzOw0KZx57NHlk4Xd2A6qXlUAnNjekbCeUi7cj95sjINBpIGjkibgIMCH1TO3iTGT5XBrwJBAKNEeqmeX5lK+chU9cmBum7uso7ak0hRlET5qSJrfEnPh8nj3mzpQ7DUQ6fWrR3s9fwCNs0uZDIxCjmj/LmTC08CQQCazSGen98F9cKFC1BXd8s1xEvvzNpkNA+k6tHGUOKsTOz8LyxQ2yehf6Z2zAivDARe2xJVrKwU4zSUGIQ1DoEfAkEAnHY4KbkKEHbUbi4d8ywvoBNQhxMQWpUNCC7sU4PcRnzNbINf+gH2dMsuiqqDCfY+vtlBDmKNj2OnTLHsO9EXuQ==";
+            String decrypted = Security.RSADecrypt(javaPrivateKey,encrypted);
+            System.out.println("Net Use Java PublicKey");
+            System.out.println(decrypted);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 }
--- src/main/java/Security.java
+++ src/main/java/Security.java
@@ -1,7 +1,18 @@
 
-import org.bouncycastle.crypto.params.RSAKeyParameters;
-import org.bouncycastle.crypto.util.PrivateKeyFactory;
+import java.security.PrivateKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+import org.apache.commons.lang3.ArrayUtils;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.security.KeyFactory;
 import javax.crypto.Cipher;
 import java.util.Base64;
 
@@ -14,22 +25,123 @@ public class Security {
 
     private final static String RSA_ALGORITHM = "RSA/ECB/PKCS1Padding";
 
-    public static String RSAEncrypt(String privateKeyJava, String data){
-        return RSAEncrypt(privateKeyJava,data,ENCODING_STRING);
+    public static String RSAEncrypt(String publicKeyJava, String data) throws Exception {
+        return rsaEncrypt(data,getPublicKey(publicKeyJava));
     }
-    public static String RSAEncrypt(String privateKeyJava, String data, String encoding)
+
+    public static String RSADecrypt(String privateKeyJava, String data) throws Exception {
+        return rsaDecrypt(data,getPrivateKey(privateKeyJava));
+    }
+
+    // 获取私钥
+    private static PrivateKey getPrivateKey(String privateKey)
     {
-        try{
-            RSAKeyParameters privateKeyParam = (RSAKeyParameters) PrivateKeyFactory.createKey(Base64.getDecoder().decode(privateKeyJava));
-            Cipher cipher = Cipher.getInstance(RSA_ALGORITHM,"BC");
-            byte[] cipherbytes = data.getBytes(encoding);
-            cipher.init(Cipher.ENCRYPT_MODE, privateKeyParam);
-            cipherbytes = cipher.DoFinal(cipherbytes, 0, cipherbytes.Length);
-            return Convert.ToBase64String(cipherbytes);
+        try {
+            byte[] keyBytes = Base64.getDecoder().decode(privateKey);
+            PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            return (RSAPrivateKey) keyFactory.generatePrivate(spec);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-        catch (Exception exception){
-            System.out.println("RSAEncrypt:"+exception);
-            return null;
+        return null;
+    }
+    // 获取公钥
+    private static PublicKey getPublicKey(String publicKey)
+    {
+        try {
+            X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(Base64.getDecoder().decode(publicKey));
+            KeyFactory factory = KeyFactory.getInstance("RSA");
+            PublicKey key = factory.generatePublic(x509EncodedKeySpec);
+            return key;
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
+        return null;
+    }
+    // 非对称加密 公钥加密
+    private static String rsaEncrypt(String text, PublicKey publicKey) throws Exception {
+        String encryptedText = "";
+        Cipher cipher = Cipher.getInstance(RSA_ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] dataReturn = null;
+        byte[] data = text.getBytes(ENCODING_STRING);
+        dataReturn = cipher.doFinal(data);
+        encryptedText = org.bouncycastle.util.encoders.Base64.toBase64String(dataReturn);
+        return encryptedText;
+    }
+    // 非对称解密 私钥解密
+    private static String rsaDecrypt(String text, PrivateKey privateKey) throws Exception {
+        Cipher cipher = Cipher.getInstance(RSA_ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        StringBuilder sb = new StringBuilder();
+        byte[] data = Base64.getDecoder().decode(text);
+        if (data.length <= 128) {
+            sb.append(new String(cipher.doFinal(data)));
+        } else {
+            for (int i = 0; i < data.length; i += 128) {
+                byte[] piece = ArrayUtils.subarray(data, i, i + 128);
+                byte[] doFinal = cipher.doFinal(piece);
+                sb.append(new String(doFinal, ENCODING_STRING));
+            }
+        }
+        return sb.toString();
+    }
+
+    // 解密数据
+    public static String desDecrypt(String message, String key) throws Exception {
+
+        byte[] bytesrc = convertHexString(message);
+        Cipher cipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
+        DESKeySpec desKeySpec = new DESKeySpec(key.getBytes(ENCODING_STRING));
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+        SecretKey secretKey = keyFactory.generateSecret(desKeySpec);
+        IvParameterSpec iv = new IvParameterSpec(key.getBytes(ENCODING_STRING));
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, iv);
+        byte[] retByte = cipher.doFinal(bytesrc);
+        return new String(retByte);
+    }
+
+
+    // 对称加密
+    public static String desEncrypt(String value ,String key) {
+        String result = "";
+        try {
+            value = java.net.URLEncoder.encode(value, ENCODING_STRING.toLowerCase());
+            Cipher cipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
+            DESKeySpec desKeySpec = new DESKeySpec(key.getBytes(ENCODING_STRING));
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+            SecretKey secretKey = keyFactory.generateSecret(desKeySpec);
+            IvParameterSpec iv = new IvParameterSpec(key.getBytes(ENCODING_STRING));
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey, iv);
+            result = toHexString( cipher.doFinal(value.getBytes(ENCODING_STRING))).toUpperCase();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return "";
+        }
+        return result;
+    }
+
+    private static byte[] convertHexString(String ss) {
+        byte digest[] = new byte[ss.length() / 2];
+        for (int i = 0; i < digest.length; i++) {
+            String byteString = ss.substring(2 * i, 2 * i + 2);
+            int byteValue = Integer.parseInt(byteString, 16);
+            digest[i] = (byte) byteValue;
+        }
+        return digest;
+    }
+    private static String toHexString(byte b[]) {
+        StringBuffer hexString = new StringBuffer();
+        for (int i = 0; i < b.length; i++) {
+            String plainText = Integer.toHexString(0xff & b[i]);
+            if (plainText.length() < 2)
+                plainText = "0" + plainText;
+            hexString.append(plainText);
+        }
+        return hexString.toString();
     }
 }
